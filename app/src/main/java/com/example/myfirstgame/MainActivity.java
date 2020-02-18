2ac93e132a7f7fb639bc6dbf9e8e1d592b19a95d
Sapphire2664 <aditya.keshan2019@vitstudent.ac.in>
2020-02-18 20:22:56 +0530

    Initial Work18/02/2020

---
+++ app/src/main/java/com/example/myfirstgame/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myfirstgame;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public void foder(View view)
+    {
+        int active=0;
+        int win;
+        int b[][]={{2,2,2},{2,2,},{2,2,2}};
+        ImageView t=(ImageView)view;
+
+    }
+    int checker(int c[][],int d)
+    {
+        
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); int active=0;
+        int b[][]={{2,2,2},{2,2,},{2,2,2}};
+
+
+    }
+}
